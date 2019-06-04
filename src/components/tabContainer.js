485279395ab9eac0f6ee7d17bbad224adeb6abc3
Darren Inglis <darren.inglis@gmail.com>
2019-06-04 14:39:07 +1000

    Update tabContainer.js

--- src/components/tabContainer.js
+++ src/components/tabContainer.js
@@ -1,63 +1,63 @@
-import React, { Component } from "react";
-import TabContent1 from "./tabContent1";
-import TabContent2 from "./tabContent2";
-import TabContent3 from "./tabContent3";
-import Tab from "./tab";
+import React, { Component } from 'react';
+import TabContent1 from './tabContent1';
+import TabContent2 from './tabContent2';
+import TabContent3 from './tabContent3';
+import Tab from './tab';
 
 export default class TabContainer extends Component {
 	state = {
-		firstActive: true,
-		secondActive: false,
-		thirdActive: false,
+		firstActive: false,
+		secondActive: true,
+		thirdActive: false
 	};
 
-	setFirstActive = e => {
+	setFirstActive = (e) => {
 		this.setState({
 			firstActive: true,
 			secondActive: false,
-			thirdActive: false,
+			thirdActive: false
 		});
 	};
 
-	setSecondActive = e => {
+	setSecondActive = (e) => {
 		this.setState({
 			firstActive: false,
 			secondActive: true,
-			thirdActive: false,
+			thirdActive: false
 		});
 	};
 
-	setThirdActive = e => {
+	setThirdActive = (e) => {
 		this.setState({
 			firstActive: false,
 			secondActive: false,
-			thirdActive: true,
+			thirdActive: true
 		});
 	};
 
 	render() {
 		return (
 			<div>
-				<section className="tabs">
-					<div className="container">
+				<section className='tabs'>
+					<div className='container'>
 						<Tab
-							id="tab-1"
-							icon="fas fa-door-open fa-3x"
-							text="Cancel anytime"
+							id='tab-1'
+							icon='fas fa-door-open fa-3x'
+							text='Cancel anytime'
 							isActive={this.state.firstActive}
 							onClick={() => this.setFirstActive()}
 						/>
 						<Tab
-							id="tab-2"
-							icon="fas fa-tablet-alt fa-3x"
-							text="Watch anywhere"
+							id='tab-2'
+							icon='fas fa-tablet-alt fa-3x'
+							text='Watch anywhere'
 							isActive={this.state.secondActive}
 							onClick={() => this.setSecondActive()}
 						/>
 						<Tab
-							id="tab-3"
-							icon="fas fa-tags fa-3x"
-							text="Pick your price"
+							id='tab-3'
+							icon='fas fa-tags fa-3x'
+							text='Pick your price'
 							isActive={this.state.thirdActive}
 							onClick={() => this.setThirdActive()}
 						/>
